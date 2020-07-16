4af866de8c2f78db70a27cf5f121e016a03e4783
victorheath <victor.iath@hotmail.com>
2020-07-16 11:36:04 -0300

    exercicios dentro do js

---
+++ projeto-final/javaScript.js
@@ -0,0 +1,127 @@
+/* O que são Variáveis e Constantes?
+Variáveis (var) é um espaço de memória onde podemos armazenar dados que podem ser alterados.
+    var pessoa = "nome" --> posso mudar o nome, pois está dentro dessa variável
+Constantes (const) também é um espaço de memória onde podemos armazenar dados, porém, não se pode alterar.
+    const pessoa = "nome" --> não posso mudar o nome que está dentro dessa constante.
+    */
+
+
+
+
+    /* COMANDOS DE DECISÃO  
+    São usados para dar direção ao código e tomar decisões baseadas no resultado lógico das expressões.
+        {
+        var idade = 20;
+        if(idade == 10)
+        {
+            alert(" idade vale 10")
+        }
+        else if(idade > 30)
+        {
+            alert("a idade é maior que 30")
+        }
+        else
+        {
+            alert( a idade é diferente de 10 e menor do que 30")
+        }
+        */
+
+
+
+        /* LOOP
+        É um código que vai se repetindo até que determinada condição seja alcançada. Se não tem condição vira um loop infinito >.<
+            while: testa as condições até a condição ser avaliada como true.
+                ex while (condição) {
+                    acão;
+                }
+            for: também são pra testar e repetir os laços, mas precisa usar a variável, a condição e incrementar OU decrementar o valor da variável inicial.
+                for (varável inicial; condição; incremento ou decremento){
+                    Ações;
+                }
+                */
+
+
+
+                /* FUNÇÕES
+                É um conjunto de instruções que serve pra executar uma tarefa.
+                ex:
+                    function Falar() {oi, olá, hello, hi}
+                    */
+
+
+
+                    /* ESCOPOS
+                        É o que determina quais são os dados que podem ser acessados em uma determinada parte do código.
+                        ex: sempre que defino uma função
+                            function  Falar() {
+                                aqui dentro é um escopo
+                            }
+                            */
+
+
+
+                        /* INPUTS E OUTPUTS
+                        Inputs são entradas de dados e Outputs são saídas, óbvio, de dados.
+                        Ex:
+                            var nome = prompt("victor");
+                            if (nome = null){
+                                document.getElementById ("seja bem vindo")
+                            } // ao colocar o nome o usuário faz uma entrada de dados e tem uma resposta com uma saída de dados.//
+                            /*
+
+
+                            /*
+                            ARRAY []
+                            Var que pode armazenar mais de 1 valor.
+                            ex:
+                                var a = [array1, array2, array3];
+                                */
+
+
+
+                                /* OBJETOS
+                                É a representação geral de "alguma coisa."
+                                São usados para incluir propriedades.
+                                ex:
+                                    var corpo = ["braços", "pernas", "etc"];
+                                */
+
+
+
+                               /* camelCase, PascalCase, kibab-case, snake_case
+
+                                    camelCase- primeira letra em minusculo e próxima palavra em maiusculo.
+                                    PascalCase- primeira letra maiusculo e próxima palavra em maiusculo.
+                                    kibab-case- inicia-se em minusculo e usa-se o traço para dar espaço.
+                                    snake_case-  inicia-se em minusculo e usa-se o umderline para dar espaço.
+                                    */
+
+
+
+
+                                    /* Diferença entre caminho absoluto e caminho relativo
+                                    Caminho relativo parte da pasta que voce está no momento e caminho absoluto seria um caminho partindo do diretório raiz.
+                                    */
+
+
+                                    /* Diferença entre os operadores &&, ||, == e != 
+
+                                    &&- retorna true se tanto uma condição quanto a outra forem verdadeiros (e)
+                                    ||- retorna true se pelo menos uam condição for
+                                    == representa igualdade
+                                    != representa diferente
+                                    */
+
+
+
+
+                                    /* EPER- preciso entender o problema, para depois planejar com cuidado e começar a executar com criatividade.. no final reviso tudo.
+
+
+
+                            /* Pilha onde o 1º que entra e o 1º que sai
+                                Fila onde o 1º que entra é ultimo que sai
+
+
+
+                        
